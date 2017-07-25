0fedc4406e409f0c7554c73bf4a35cccb3c05830
Robby Bourne <robbybourne1@gmail.com>
2017-07-25 14:01:30 -0400

    Hello GoodBye App

--- JS_Projects/express-practice/express.js
+++ JS_Projects/express-practice/express.js
@@ -2,9 +2,18 @@ const express = require('express')
 
 const app = express()
 
-app.get('/' (request, response) => {
-  response.send('Hello World')
-  app.listen(3000, () =>{
-    console.log('Listening on port 3000')
-  })
+app.get('/', (request, response) => {
+  response.send('Hello/ GoodBye App ' + '<a href= "/goodbye"<div>GoodBye</div>' + ' <a href="/hello"<div>Hello</div>')
+})
+
+app.get('/goodbye', (request, response) => {
+  response.send('GoodBye!! Seeyaa Later' + ' <a href="/"<div>Home</div>')
+})
+
+app.get('/hello', (request, response) => {
+  response.send('Hello!! Welcome :)' + ' <a href="/"<div>Home</div>')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
 })
