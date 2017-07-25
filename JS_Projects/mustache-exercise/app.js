991de898e516a6231decf62b3f4fe81a253ee6ad
Robby Bourne <robbybourne1@gmail.com>
2017-07-25 15:12:59 -0400

    Li and Form Element Added

--- JS_Projects/mustache-exercise/app.js
+++ JS_Projects/mustache-exercise/app.js
@@ -9,8 +9,16 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 //Listening on root
+// app.get('/', function(req, res) {
+//   res.render('todo')
+// })
+
 app.get('/todo/', function(req, res) {
-  // TODO write your code here
+  const data = {
+    listName: 'TODO',
+    list: [{ item: 'Learn Node' }, { item: 'Learn Express' }, { item: 'Learn Mustache' }]
+  }
+  res.render('todo', data)
 })
 
 app.listen(3000, function() {
